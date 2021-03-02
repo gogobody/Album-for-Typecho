f3497024bd858cbfe267ad6cd9fc32a76c2cb85a
gogobody <138950128@qq.com>
2021-03-02 10:02:08 +0800

    init

---
+++ Album/Data/Baseconfig.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Album For Typecho, base on 1.0/14.10.10
+ *
+ * @package Album Base Config
+ * @author 我本奈何 modify by gogobody
+ * @version 1.2 beta
+ * @link https://plsYou.com
+ */
+
+$app_welcome = '当前版本 1.2 hhh test';
+$read_order = 'ASC';
+$template = '/Default/';
+$category_show = 'no';
+$per_page_num = '16';
+$page_nav_show = 'yes';
+$ajax_Calendar = 'normal';
---
+++ Album/Template/Default/Footer.php
@@ -0,0 +1,16 @@
+
+            
+        </div><!-- end .colgroup -->
+    </div>
+</div><!-- end #body -->
+
+<footer id="footer"  role="contentinfo">
+	<div class="container">
+    &copy; <?php _e(date('Y')); ?> <a href="<?php _e($siteUrl); ?>"><?php _e($title); ?></a> | 
+    <?php _e('Plugin by <a href="https://plsYou.com">plsYou</a> '); ?> | 
+    <?php _e( ' Run in '.$run_time.'s'); ?>
+  </div>
+</footer><!-- end #footer -->
+<script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
+</body>
+</html>
